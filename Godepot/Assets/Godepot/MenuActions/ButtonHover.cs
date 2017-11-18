be1def05a83275d1045c1f7b436557306d94da02
Kenjiro Goodson <kcgoodson@gmail.com>
2017-11-18 15:55:13 -0800

    GodepotBackup
    Create The GitBackup

---
+++ Godepot/Assets/Godepot/MenuActions/ButtonHover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonHover : MonoBehaviour {
+
+	SpriteRenderer s;
+	public Color[] buttonColor;
+
+	void Start() {
+		s = GetComponent<SpriteRenderer>();
+	}
+
+	void OnMouseEnter() {
+		s.color = FixColor(1);
+	}
+
+	void OnMouseExit(){
+		s.color = FixColor(0);
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown() {
+		s.color = FixColor(2);
+	}
+
+	void OnMouseUp() {
+		s.color = FixColor(0);
+	}
+
+	Color FixColor(int i) {
+		Color pre = buttonColor[i];
+		return new Color(pre.r, pre.g, pre.b, 1);
+	}
+}
