ae422beecd251f6e3dc2adba7106bf652dc6092b
Kenjiro Goodson <kcgoodson@gmail.com>
2017-11-20 13:35:40 -0800

    MoreGreek

---
+++ Godepot/Assets/Godepot/Sprites/TreeTrick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTrick : MonoBehaviour {
+
+	public GameObject backTree;
+	bool isSet = false;
+
+	public void SetTree() {
+		if (!isSet) {
+			isSet = true;
+			backTree.GetComponent<SpriteRenderer> ().enabled = true;
+		}
+	}
+}
